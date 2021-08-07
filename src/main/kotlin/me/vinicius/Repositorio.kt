cbf4319c413c348cdaca46c27802c55a4ff3c4fb
ViniHering <vinicius-heringer@hotmail.com>
2021-08-07 16:39:21 -0300

    created repositorio class

---
+++ src/main/kotlin/me/vinicius/Repositorio.kt
@@ -0,0 +1,12 @@
+package me.vinicius
+
+class Repositorio<T> {
+    private val map = mutableMapOf<String, T>()
+
+    fun create(id: String, value: T) {
+        map[id] = value
+    }
+
+    fun findById(id: String) = map[id]
+
+}
